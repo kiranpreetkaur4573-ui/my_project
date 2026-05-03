e0f74d853a389262a23cb2996f2ac6fd407c8121
kirti <kiranpreetkaur4573@gmail.com>
2026-05-04 05:16:03 +0530

    final working project

--- server.js
+++ server.js
@@ -37,3 +37,6 @@ app.get('/api/test', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'main_dash.html'));
+});
